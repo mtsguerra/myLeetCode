05ff1877aff784d08fdffbcd65b551a84901c569
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-28 13:14:36 +0000

    docs: improve comments for Two Sum solution (ex0001)

--- java/ex0001.java
+++ java/ex0001.java
@@ -2,18 +2,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class ex0001 {
-
-    //--------------------------------------------------------------------//
-    // in this one to buff the efficiency of this code i will be using a hashMap so i can
-    // store the current number and the position where it is, so while iterating through 
-    // the array i will be creating a new target that will be the complement to the current
-    // number so that the sum of both can result on the target, and check if this new target
-    // is present on the hashSet, in the case it is present i will be returning the current index
-    // and the value of the key==new target, where value = index of the complement, where
-    // it will always be in order so just return it will solve the problem
-
-    static Scanner input = new Scanner(System.in);
-
+    /**
+     * Finds two numbers in an array that add up to a target value.
+     * @param nums nums Array of integers
+     * @param target Target value to find
+     * @return An array containing the indices of the two numbers that add up to the target value.
+     */
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i=0;i<nums.length;i++){
@@ -23,14 +17,4 @@ public class ex0001 {
         }
         return null;
     }
-
-    public void read(){
-        String s = input.nextLine();
-    }
-    public static void main(String[] args) {
-        ex0001 myo = new ex0001();
-        myo.read();
-    }
-
-    //--------------------------------------------------------------------//
 }
