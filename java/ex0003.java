88214d27c99cad92f8d512fdfd0110caced38302
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-28 13:23:14 +0000

    docs: improve comments and variable naming for Longest Substring solution (ex0003)

--- java/ex0003.java
+++ java/ex0003.java
@@ -2,72 +2,34 @@ import java.util.HashSet;
 
 public class ex0003 {
 
-    //--------------------------------------------------------------------//
-    // in this one i will be using one pointer to point to the beginning of the current sequence
-    // and the other to accompany the loop, and the most important a hashset to store, the
-    // characters, so while in the looping we will be adding the characters until one character
-    // its already on the hashset, in that case we will be getting the size of the hashset to get
-    // the size of the current seq and compare it to the biggest seq, after that we will be
-    // removing the a_pointer elements of the hashset until the character that its equal to the 
-    // current character is removed, so we can start a new seq
-
+    /**
+     * Finds the length of the longest substring without repeating characters.
+     * Uses a sliding window approach with a HashSet to track characters in the current window.
+     *
+     * Time Complexity: O(n) where n is the length of the string
+     * Space Complexity: O(min(m,n)) where m is the size of the character set
+     *
+     * @param s The input string to be processed
+     * @return The length of the longest substring without repeating characters
+     */
     public int lengthOfLongestSubstring(String s){
-        int a_pointer = 0;
-        int b_pointer =0;
+        int leftPointer = 0;
+        int rightPointer =0;
         int biggestSeq =0;
 
         HashSet<Character> hashSet = new HashSet<>();
 
-        while (b_pointer < s.length()){
-            if (!hashSet.contains(s.charAt(b_pointer))){
-                hashSet.add(s.charAt(b_pointer));
-                b_pointer++;
+        while (rightPointer < s.length()){
+            if (!hashSet.contains(s.charAt(rightPointer))){
+                hashSet.add(s.charAt(rightPointer));
+                rightPointer++;
                 biggestSeq = Math.max(hashSet.size(), biggestSeq);
             }
             else{
-                hashSet.remove(s.charAt(a_pointer));
-                a_pointer++;
+                hashSet.remove(s.charAt(leftPointer));
+                leftPointer++;
             }
         }
         return biggestSeq;
-    }
-
-    //--------------------------------------------------------------------//
-
-    /* 
-    public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> currentMap = new HashMap<>();
-        int biggestSeq=0;
-        int firstIndexOfTheSeq = 0;
-        StringBuilder currentSeq = new StringBuilder();
-        for (int i = 0;i<s.length();i++){
-            if (currentMap.size() == 0){
-                firstIndexOfTheSeq = i;
-                currentMap.put(s.charAt(i), 1);
-                currentSeq = new StringBuilder();
-                currentSeq.append(s.charAt(i));
-                continue;
-            }
-            if (currentMap.containsKey(s.charAt(i))){
-                if (currentSeq.length() > biggestSeq){
-                    biggestSeq = currentSeq.length();
-                }
-                currentMap = new HashMap<>();
-                i = firstIndexOfTheSeq;
-            }
-            else {
-                currentMap.put(s.charAt(i), 1);
-                currentSeq.append(s.charAt(i));
-            }
-        }
-        if (currentSeq.length() > biggestSeq){
-            biggestSeq = currentSeq.length();
-        } 
-        return biggestSeq;
-    }*/
-
-    public static void main(String[] args) {
-        ex0003 myo = new ex0003();
-        System.out.println(myo.lengthOfLongestSubstring("dvdf"));    
     }
 }
