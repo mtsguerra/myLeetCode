2712d489df791abd563aec16b42dbdc81002051c
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-01 14:41:42 +0100

    Adding solution for the exercise Delete Greatest Value in Each Row (ex2500)

---
+++ java/ex2500.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class ex2500{
+    /**
+     * Deletes the greatest value from each row and returns the greatest
+     * value of those removed and accumulate it with +.
+     *
+     * Time Complexity : O(m*n*log(n)), m->rows, n->cols.
+     * Space Complexity : O(1) only using arrays
+     *
+     * @param grid the @D integer grid to process it
+     * @return the sum of the maximum values removed
+     */
+    public int deleteGreatestValue(int[][] grid) {
+        int ans = 0;
+        for (int i=0;i<grid.length;i++){
+            Arrays.sort(grid[i]);
+        }
+        for (int i=grid[0].length-1;i>=0;i--){
+            int crr = 0;
+            for (int j=0;j<grid.length;j++){
+                crr=Math.max(crr,grid[i][j]);
+            }
+            ans += crr;
+        }
+        return ans;
+    }
+}
