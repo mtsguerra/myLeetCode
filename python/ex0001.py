ec409fc3ecbe632bb038ccdeb8025a4c6f87d6ec
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-01 11:42:44 +0100

    Adding solution for Two Sum solution (ex0001)

---
+++ python/ex0001.py
@@ -0,0 +1,9 @@
+class ex0001(object):
+    def twoSum(self,nums,target):
+        num_map = {};
+        for i, num in enumerate(nums):
+            complement  = target - num;
+            if complement in num_map:
+                return [num_map[complement], i];
+            num_map[num] = i;
+        return [];
