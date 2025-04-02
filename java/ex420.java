f5972ca77588d33e79ab35719f750d2fd4eb3e06
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-02 21:31:08 +0100

    Adding solution for the exercise Strong Password Checker (ex420)

--- java/ex420.java
+++ java/ex420.java
@@ -10,15 +10,31 @@ class ex420{
         int nUpper = 0;
         int nDigit = 0;
 
-        List<String> sequences = new ArrayList<>();
-        StringBuilder crrSequence = new StringBuilder();
+        // size of the sequences -> how many sequences with that size
+        int[] sequences = new int[password.length()+1];
+        int nSequences =0;
+        int[] threePlus = new int[3];
+        // size of the sequences that are multiple of 3 -> how many sequences with that size
+        int[] multByThree = new int[(password.length()+1)/3+1];
+        Character crrChar = null;
+        int sizeSequence = 0;
         for (char ch : password.toCharArray()) {
-            if (crrSequence.isEmpty() || crrSequence.charAt(0)==ch) crrSequence.append(ch);
+            if (crrChar==null || ch==crrChar){
+                sizeSequence++;
+                crrChar = ch;
+            }
             else{
-                if (crrSequence.charAt(0) != ch){
-                    if (crrSequence.length() >= 3) sequences.add(crrSequence.toString());
-                    crrSequence = new StringBuilder();
-                    crrSequence.append(ch);
+                if (ch != crrChar){
+                    if (sizeSequence >= 3){
+                        nSequences++;
+                        sequences[sizeSequence]++;
+                        threePlus[sizeSequence%3]++;
+                        if (sizeSequence % 3 == 0){
+                            multByThree[sizeSequence/3]++;
+                        }
+                    }
+                    crrChar = ch;
+                    sizeSequence = 1;
                 }
             }
             if (Character.isAlphabetic(ch)){
@@ -27,7 +43,14 @@ class ex420{
             }
             else if (Character.isDigit(ch)) nDigit++;
         }
-        if (crrSequence.length() >= 3) sequences.add(crrSequence.toString());
+        if (sizeSequence >= 3){
+            nSequences++;
+            sequences[sizeSequence]++;
+            threePlus[sizeSequence%3]++;
+            if (sizeSequence % 3 == 0){
+                multByThree[sizeSequence/3]++;
+            }
+        }
         // first do the > 20
         // after that remove the characters from the sequences, if a sequence
         // gets to .size() < 3, remove it from the list
@@ -36,21 +59,69 @@ class ex420{
         // that still need to be altered
 
         // third check if we need to add characters
-        Collections.sort(sequences, Collections.reverseOrder());
-        int replaced = 0;
-        for (String str : sequences){
-            int toAlter= str.length() / 3;
-            ans+= toAlter;
-            if (nCharacters < 6) {
-                nCharacters += Math.min(toAlter, 6-nCharacters);
-                toAlter -= Math.min(toAlter, 6-nCharacters);
+        int toEliminate = 0;
+        if (nCharacters > 20) toEliminate = nCharacters - 20;
+        while (toEliminate > 0 && nSequences > 0){
+            int pos=-1;
+            if (threePlus[0] >0){
+                for (int i=0;i<multByThree.length;i++){
+                    if (multByThree[i] > 0){
+                        pos = i*3;
+                        break;
+                    }
+                }
             }
-            replaced += toAlter;
-            while (toAlter > 0 && (nLower == 0 || nUpper == 0 || nDigit == 0)){
+            else {
+                int startPoint = -1;
+                if (threePlus[1]>0) startPoint=4;
+                else startPoint = 5;
+                for (int i=startPoint;i<sequences.length;i+=3){
+                    if (sequences[i] > 0){
+                        pos = i;
+                        break;
+                    }
+                }
+            }
+            ans++;
+            sequences[pos]--;
+            threePlus[pos%3]--;
+            toEliminate--;
+            if (pos<=3) nSequences--;
+            else {
+                sequences[pos-1]++;
+                threePlus[(pos-1)%3]++;
+                if ((pos-1) % 3 == 0){
+                    multByThree[(pos-1)/3]++;
+                }
+            }
+            if (pos % 3 == 0){
+                multByThree[pos/3]--;
+            }
+        }
+        // in case there's still characters to eliminate
+        ans+= toEliminate;
+        // now we need to replace the characters
+        while (nSequences > 0){
+            int pos = -1;
+            for (int i=3;i<sequences.length;i++){
+                if (sequences[i] > 0){
+                    pos = i;
+                    break;
+                }
+            }
+            if (pos == -1) break;
+            int toChange = pos/3;
+            ans += toChange;
+            if (nCharacters < 6){
+                nCharacters+= Math.min (toChange, 6 - nCharacters);
+            }
+            sequences[pos]--;
+            if (pos <= 3) nSequences--;
+            while (toChange > 0 && (nLower == 0 || nUpper == 0 || nDigit == 0)){
                 if (nLower == 0) nLower++;
                 else if (nUpper == 0) nUpper++;
                 else if (nDigit == 0) nDigit++;
-                toAlter--;
+                toChange--;
             }
         }
         while (nCharacters < 6) {
@@ -58,10 +129,6 @@ class ex420{
             else if (nUpper == 0) nUpper++;
             else if (nDigit == 0) nDigit++;
             nCharacters++;
-            ans++;
-        }
-        while (nCharacters > 20){
-            nCharacters--;
             ans++;
         }
         while (nLower == 0 || nUpper == 0 || nDigit == 0) {
@@ -75,7 +142,7 @@ class ex420{
 
     public static void main(String[] args) {
         ex420 myObj = new ex420();
-        String str = "bbaaaaaaaaaaaaaaacccccc";
+        String str = "QQQQQ";
         int p = myObj.strongPasswordChecker(str);
         System.out.println("The result is: " + p);
     }
