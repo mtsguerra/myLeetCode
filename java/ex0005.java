c388b59d815a81bbaed35d510c40ce14025d130e
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-03 20:54:29 +0100

    docs: improve comments and variable naming for Longest Palindrome Substring (ex0005)

--- java/ex0005.java
+++ java/ex0005.java
@@ -1,40 +1,43 @@
 public class ex0005 {
 
-    //--------------------------------------------------------------------//
-    // on this one we will be starting with two variables to store the start and the end of
-    // the palindrome, indexs wise, after that we will iterate through the string, and trying
-    // for palindromes, the method used to find the size of the palindrome its one that
-    // starts in the middle (current index) and expanding to the sizes until its not a palindrome
-    // anymore or if the indexes get to the boundaries, so returning to the first loop, we
-    // will use this method two times, one to find the possible even palindrome and the other
-    // for the not even palindrome, and we will use the biggest of both to compare to the current
-    // palindrome size, defined by the end - start, and in the case of the current palindrome being
-    // bigger we will alter the start and end, in the start we will be decreasing 1 from the len
-    // because of the case of the palindrome being even we only need to decrease len-1/2,
-    // and in the case of not being even the decreasing one from the len would not alter the result
-    // now on the substring we add one to the end because the boundary must be until the end+1,
-    // so we do not cut out the last character, format being [start,end[
+    /**
+     * Given a string s, return the longest palindromic substring in s.
+     *
+     * Time complexity: O(n^2)
+     * Space complexity: O(1)
+     *
+     * @param s The input string
+     * @return The longest palindromic substring
+     */
+    public String longestPalindrome(String s){
+        if (s==null || s.isEmpty()) return "";
 
-    public String longestPalindrome(String s) {
-        if (s==null || s.length() < 1) return "";
+        int left = 0;
+        int right = 0;
 
-        int start=0;
-        int end = 0;
-        
         for (int i=0;i<s.length();i++){
-            int len1 = expandFromMiddle(s, i, i);
-            int len2 = expandFromMiddle(s, i, i+1);
-            int len = Math.max(len1, len2);
-            if (len > end-start){
-                start = i - ((len-1)/2);
-                end = i + (len/2);
+            int maxLen = Math.min(i+1,s.length()-i)*2+1;
+            if (maxLen < right-left) continue;
+            int lenOdd = expandFromMiddle(s, i, i);
+            int lenEven = expandFromMiddle(s, i, i+1);
+            int len = Math.max(lenOdd, lenEven);
+            if (len > right-left){
+                left = i - ((len-1)/2);
+                right = i + (len/2);
             }
+
         }
-
-        return s.substring(start, end+1);
-
+        return s.substring(left,right+1);
     }
 
+    /**
+     * Expands from the middle of the string to find the length of the palindrome.
+     *
+     * @param s The input string
+     * @param left The left index
+     * @param right The right index
+     * @return The length of the palindrome
+     */
     public int expandFromMiddle (String s, int left, int right){
         if (s==null || left > right) return 0;
         while (left >=0 && right < s.length() && s.charAt(left)==s.charAt(right)){
@@ -44,10 +47,8 @@ public class ex0005 {
         return right - left - 1;
     }
 
-    //--------------------------------------------------------------------//
-
     public static void main(String[] args) {
         ex0005 myo = new ex0005();
-        System.out.println(myo.longestPalindrome("abba"));
+        System.out.println(myo.longestPalindrome("babad"));
     }
 }
