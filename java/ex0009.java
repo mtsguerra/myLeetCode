c90cf33a6cb4fa5a87fd7e1eee92cb196eefe73b
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-03 23:24:29 +0100

    docs: improve comments for Palindrome Number (ex0009)

--- java/ex0009.java
+++ java/ex0009.java
@@ -1,11 +1,22 @@
 public class ex0009 {
 
-    //---------------------------------------//
-    // first check if the number is negative or if its devisable by 10 and diff 0, after that we create a number thar will be the reversed number
-    // while we eliminate the x%10 from the original we append it to the reversed and while doing it we multiply by ten when adding a new one,
-    // so in the final we check if the numbers are equal, if they are not theres still a chance of the number be palindrome but with a even number
-    // of numbers so we test it again but without the number on the middle
-
+    /**
+     * Checks if an integer is a palindrome.
+     *
+     * Approach:
+     * 1. If the number is negative or ends with 0 (and is not 0), it cannot
+     * be a palindrome.
+     * 2. Initialize a variable to store the reversed integer.
+     * 3. Use a loop to extract the last digit of the integer and append it
+     * to the reversed integer.
+     * 4. Check if the original number is equal to the reversed number or
+     *
+     * Time complexity: O(log n) where n is the number of digits in x.
+     * Space complexity: O(1) since we are using a constant amount of space.
+     *
+     * @param x the integer to be checked
+     * @return true if x is a palindrome, false otherwise
+     */
     public boolean isPalindrome(int x) {
         if (x<0 || (x%10==0 && x!=0)) return false;
         int reversed = 0;
@@ -16,22 +27,6 @@ public class ex0009 {
         return x == reversed || x == reversed / 10;
     }
 
-    //---------------------------------------//
-
-/*public boolean isPalindrome(int x) {
-    if (x<0) return false;
-    String str = String.valueOf(x);
-    if (str.charAt(str.length()-1) == '-') return false;
-    if (str.length() == 1) return true;
-    int r=str.length()-1;
-    int l=0;
-    while (l < str.length() && r>=0 &&r >= l){
-        if (str.charAt(l) != str.charAt(r)) return false;
-        l++;
-        r--;
-    }
-    return true;
-}*/
     public static void main(String[] args) {
         ex0009 myo = new ex0009();
         myo.isPalindrome(121);
