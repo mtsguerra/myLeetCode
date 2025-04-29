2418db398facfb977adb04b56b4876694073b921
mtsguerra7 <mtsguerra9@gmail.com>
2025-04-29 13:18:06 +0100

    docs: improve comments for Remove Nth Node From End of List (ex0019)

--- java/ex0019.java
+++ java/ex0019.java
@@ -1,4 +1,19 @@
 public class ex0019 {
+    /**
+     * Removes the nth node from the end of a linked list.
+     *
+     * This method uses a two-pointer technique to find the node to be removed.
+     * The first pointer traverses the list to find its length,
+     * and the second pointer traverses the list again
+     * to find the node to be removed.
+     *
+     * Time Complexity: O(n), where n is the length of the linked list.
+     * Space Complexity: O(1), since we are using a constant amount of space.
+     *
+     * @param head the head of the linked list
+     * @param n the position from the end of the list to remove
+     * @return the head of the modified linked list
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode current = head;
         ListNode last = null;
