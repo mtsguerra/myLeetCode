05dfab33b7d4f74eea32d657a330417c275bc0ec
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-16 15:07:26 +0100

    Adding solution for Find the Maximum Length of Valid Subsequence I

---
+++ java/ex3201.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ex3201 {
+
+    public int maximumLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int nEven=0;
+        int nOdd=0;
+        int altSeq = 1;
+        int last = nums[0];
+        if (nums[0]%2==0) nEven++;
+        else nOdd++;
+        for (int i=1;i<nums.length;i++){
+            if (nums[i]%2==0) nEven++;
+            else nOdd++;
+            if (nums[i]%2!=last%2){
+                altSeq++;
+                last = nums[i];
+            }
+        }
+        return Math.max(Math.max(nOdd,nEven), altSeq);
+    }
+
+    public static void main(String[] args) {
+        int[] s = {1,2,1,1,2,1,2};
+        ex3201 myo = new ex3201();
+        System.out.println(myo.maximumLength(s));
+    }
+}
