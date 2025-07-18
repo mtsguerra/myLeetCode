da4c8ce26178ddc0d3d3c158e9145e076bbe2ff0
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-18 23:38:31 +0100

    Organizing some new folders

--- java/ArrayHashTable/ex0001.java
+++ java/ArrayHashTable/ex0001.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class ex0001 {
     /**
--- java/String/ex0014.java
+++ java/String/ex0014.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class ex0014 {
 
     /**
