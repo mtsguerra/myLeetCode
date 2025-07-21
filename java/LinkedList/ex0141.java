953b75b47c811424c0660a7bf570a5b94dc943d4
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-21 01:41:12 +0100

    Linked List Cycle - Adding solution using Floyd's tortoise and hare

---
+++ java/LinkedList/ex0141.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ex0141 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
+
+    /*
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode current = head;
+        while (current != null) {
+            if (set.contains(current)) return true;
+            set.add(current);
+            current = current.next;
+        }
+        return false;
+    }/*
+}
