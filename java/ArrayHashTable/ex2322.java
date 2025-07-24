dacc163297efdf832746b129120bb9c18b2ddcf2
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-24 19:31:15 +0100

    Minimum Score After Removals on a Tree - Adding a optimized solution

--- java/ArrayHashTable/ex2322.java
+++ java/ArrayHashTable/ex2322.java
@@ -12,6 +12,79 @@ class ex2322{
     public int minimumScore(int[] nums, int[][] edges) {
         @SuppressWarnings("unchecked")
         List<Node> [] graph = new ArrayList[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(new Node(edge[1]));
+            graph[edge[1]].add(new Node(edge[0]));
+        }
+        int[] xor = new int[nums.length];
+        int[] in = new int[nums.length];
+        int[] out = new int[nums.length];
+        boolean[] visited = new boolean[nums.length];
+        int[] time = new int[1];
+        dfs(xor, in, out, graph, nums, visited, 0,-1, time);
+
+        int lowestScore = Integer.MAX_VALUE;
+
+        for (int i = 0; i < edges.length; i++) {
+            for (int j = i + 1; j < edges.length; j++) {
+
+                int[] edge1 = edges[i];
+                int[] edge2 = edges[j];
+
+                int a = (in[edge1[0]] > in[edge1[1]]) ? edge1[0] : edge1[1];
+                int b = (in[edge2[0]] > in[edge2[1]]) ? edge2[0] : edge2[1];
+
+                boolean isParentEd1 = isAncestor(in, out, a, b);
+                boolean isParentEd2 = isAncestor(in, out, b, a);
+
+                int xor1 = xor[0];
+                int xor2 = xor[a];
+                int xor3 = xor[b];
+                if (isParentEd1) {
+                    xor1 ^= xor2;
+                    xor2 ^= xor3;
+                }
+                else if (isParentEd2) {
+                    xor1 ^= xor3;
+                    xor3 ^= xor2;
+                }
+                else {
+                    xor1 ^= xor2 ^ xor3;
+                }
+                int biggest = Math.max(Math.max(xor1, xor2), xor3);
+                int smallest = Math.min(Math.min(xor1, xor2), xor3);
+                int score = biggest - smallest;
+                lowestScore = Math.min(lowestScore, score);
+            }
+        }
+        return lowestScore;
+    }
+
+    boolean isAncestor(int[] in, int[] out, int parent, int child) {
+        return in[parent] <= in[child] && out[parent] >= out[child];
+    }
+
+    private void dfs (int[] xor, int[] in, int[] out, List<Node>[] graph,
+                      int[] nums, boolean[] visited, int node,
+                      int parent, int[] time) {
+        in[node] = time[0]++;
+        xor[node] = nums[node];
+        for (Node child : graph[node]) {
+            if (child.to != parent){
+                dfs(xor, in, out, graph, nums, visited, child.to, node, time);
+                xor[node] ^= xor[child.to];
+            }
+        }
+        out[node] = time[0]++;
+    }
+
+    /*
+    public int minimumScore(int[] nums, int[][] edges) {
+        @SuppressWarnings("unchecked")
+        List<Node> [] graph = new ArrayList[nums.length];
         int[] level = new int[nums.length];
         Arrays.fill(level, Integer.MAX_VALUE);
         level[0] = 0;
@@ -106,12 +179,12 @@ class ex2322{
             }
         }
         return lowestScore;
-    }
+    }*/
 
     public static void main(String[] args) {
         ex2322 solution = new ex2322();
-        int[] nums = {29,29,23,32,17};
-        int[][] edges = {{3,1},{2,3},{4,1},{0,4}};
+        int[] nums = {9,14,2,1};
+        int[][] edges = {{2,3},{3,0},{3,1}};
         int result = solution.minimumScore(nums, edges);
         System.out.println("Minimum Score: " + result);
     }
