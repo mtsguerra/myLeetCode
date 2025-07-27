ce9c5548e1ac8722d5091a06c11a1a2e4e49473f
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-28 00:59:56 +0100

    Longest Consecutive Sequence - Adding comments

--- java/ArrayHashTable/ex0128.java
+++ java/ArrayHashTable/ex0128.java
@@ -1,6 +1,19 @@
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 
 class ex0128{
+
+    /**
+     * Given an unsorted array of integers nums, return the length of the
+     * longest consecutive elements sequence.
+     *
+     * Time Complexity: O(n) where n is the number of elements in the array.
+     * Space Complexity: O(n) for storing elements in a HashSet.
+     *
+     * @param nums Array of integers
+     * @return Length of the longest consecutive elements sequence
+     */
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
         HashSet<Integer> set = new HashSet<>();
@@ -8,7 +21,7 @@ class ex0128{
             set.add(n);
         }
         int longest = 1;
-        for (int n :set){
+        for (int n : nums){
             if (!set.contains(n-1)){
                 int streak = 1;
                 while (set.contains(n+streak)){
