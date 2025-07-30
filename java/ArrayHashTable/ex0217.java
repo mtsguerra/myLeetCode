1a3d55cf78de40cf039c2b79fd96d4ce83a6dd84
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-30 12:48:32 +0100

    Contains Duplicate - Adding comments

--- java/ArrayHashTable/ex0217.java
+++ java/ArrayHashTable/ex0217.java
@@ -1,6 +1,16 @@
 import java.util.HashSet;
 
 class ex0217 {
+    /**
+     * Given an integer array nums, return true if any value appears at least
+     * twice in the array, and return false if every element is distinct.
+     *
+     * Time Complexity: O(n) where n is the length of the input array.
+     * Space Complexity: O(n) for storing elements in a HashSet.
+     *
+     * @param nums Array of integers
+     * @return true if there are duplicates, false otherwise
+     */
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int i:nums){
