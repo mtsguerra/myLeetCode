24642736f4d635e8874a02e545f9b9a6c5a69149
Matheus Guerra <mtsguerra9@gmail.com>
2025-07-30 12:56:15 +0100

    Product of Array Except Self - Improving code efficiency and comments

--- java/ArrayHashTable/ex0238.java
+++ java/ArrayHashTable/ex0238.java
@@ -1,5 +1,39 @@
 public class ex0238 {
+
+    /**
+     * Given an integer array nums, return an array answer such that
+     * answer[i] is equal to the product of all the elements of nums except nums[i].
+     *
+     * Time Complexity: O(n) where n is the length of the input array.
+     * Space Complexity: O(n) for the output array.
+     *
+     * @param nums Array of integers
+     * @return Array of products except self
+     */
     public int[] productExceptSelf(int[] nums) {
+        int countZero = 0;
+        int posZero = -1;
+        int product = 1;
+        int[] ans = new int[nums.length];
+        for (int i=0;i<nums.length;i++){
+            int num = nums[i];
+            if (num==0) {
+                if (++countZero > 1) return ans;
+                posZero = i;
+            }
+            else product *= num;
+        }
+        if (countZero == 1) {
+            ans[posZero] = product;
+            return ans;
+        }
+        for (int i=0;i<nums.length;i++){
+            ans[i] = product / nums[i];
+        }
+        return ans;
+    }
+
+    /*public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
         int[] sumsL = new int[nums.length];
         int[] sumsR = new int[nums.length];
@@ -13,7 +47,7 @@ public class ex0238 {
             ans[i] = sumsL[i-1]*sumsR[i+1];
         }
         return ans;
-    }
+    }*/
 
     public static void main(String[] args) {
         ex0238 myo = new ex0238();
