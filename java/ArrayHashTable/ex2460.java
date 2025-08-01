1feab738b9ad71f31c927f18c90b31ebd76f2f7c
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-01 16:32:34 +0100

    Apply Operations to an Array - Adding comments

--- java/ArrayHashTable/ex2460.java
+++ java/ArrayHashTable/ex2460.java
@@ -1,4 +1,14 @@
 class ex2460{
+    /**
+     * Given a zero-based array nums of size n, for each i in the range 0 <= i < n - 1,
+     * if nums[i] == nums[i + 1], then nums[i] is set to 2 * nums[i] and nums[i + 1] is set to 0.
+     *
+     * Time Complexity: O(n), where n is the length of the input array.
+     * Space Complexity: O(n) for the output array.
+     *
+     * @param nums Array of integers
+     * @return An array of integers after applying the operations.
+     */
     public int[] applyOperations(int[] nums) {
         int[] ans = new  int[nums.length];
         int pos = 0;
