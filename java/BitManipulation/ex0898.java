db8e7b99a7c34b547e32e796167ea0b4aa943708
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-01 14:58:14 +0100

    Bitwise ORs of Subarrays - Couldnt find a better solution

--- java/BitManipulation/ex0898.java
+++ java/BitManipulation/ex0898.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 class ex0898 {
 
+    /**
+     * Given an array of integers arr, return the number of distinct bitwise OR results
+     * of all non-empty subarrays of arr.
+     * Approach:
+     * This solution uses a HashSet to keep track of distinct bitwise OR results.
+     * It iterates through the array in reverse order, maintaining a set of
+     * only the distinct bitwise OR results of the last processed index.
+     *
+     * Time Complexity: O(n^2) in the worst case, but optimized to O(n) on average.
+     * Space Complexity: O(n) for storing distinct results.
+     *
+     * @param arr Array of integers
+     * @return Number of distinct bitwise OR results
+     */
     public int subarrayBitwiseORs(int[] arr) {
         HashSet<Integer> set = new HashSet<>();
         HashSet<Integer> prev = new HashSet<>();
