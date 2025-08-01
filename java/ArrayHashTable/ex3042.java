691db6a3bb59717ffecfc69f0d31d377fd36c305
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-01 16:34:44 +0100

    Count Prefix and Suffix Pairs I - Adding comments

--- java/ArrayHashTable/ex3042.java
+++ java/ArrayHashTable/ex3042.java
@@ -2,6 +2,17 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class ex3042 {
+    /**
+     * Given an array of strings words, return the number of pairs (i, j)
+     * such that i < j and words[i] is a prefix of words[j] and
+     * words[i] is a suffix of words[j].
+     *
+     * Time Complexity: O(n^2 * m) where n is nWords and m is the avg len(words)
+     * Space Complexity: O(1) for the result variable.
+     *
+     * @param words Array of strings
+     * @return Number of prefix-suffix pairs
+     */
     public int countPrefixSuffixPairs(String[] words) {
         int result=0;
         Arrays.sort(words, Comparator.comparingInt(String::length));
