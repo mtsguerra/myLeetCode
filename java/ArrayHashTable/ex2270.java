030f3231f7ab67ebaa7b24d8389d365094cc847c
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-01 16:28:14 +0100

    Number of Ways to Split Array - Fixing comments

--- java/ArrayHashTable/ex2270.java
+++ java/ArrayHashTable/ex2270.java
@@ -1,16 +1,23 @@
 public class ex2270 {
+    /**
+     * Given an integer array nums, return the number of ways you can split
+     * the array into two non-empty parts such that the sum of the elements
+     * in the first part is greater than or equal to the sum of the elements
+     * in the second part.
+     *
+     * Time Complexity: O(n), where n is the length of the input array.
+     * Space Complexity: O(1), as we are using only a few variables for counting and tracking.
+     *
+     * @param nums an array of integers
+     * @return the number of valid splits
+     */
     public int waysToSplitArray(int[] nums) {
-        // start it with long to avoid overflow cases
         long totalSum =0;
-        // calculate the sum of the whole array
         for (int n : nums) totalSum+=n;
-        // variable to store the current sum of the split
         long currentSum =0;
         int result = 0;
         for (int i=0;i<nums.length-1;i++){
-            // increases the sum with the current number
             currentSum += nums[i];
-            // gets the other part of the split by doing total sum - current sum
             if (currentSum >= totalSum-currentSum) result++;
         }
         return result;
