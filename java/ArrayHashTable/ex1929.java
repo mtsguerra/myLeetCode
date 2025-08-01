bb995b90c55458a3e10398350cd7594770f09266
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-01 16:24:12 +0100

    Concatenation of Array - Adding comments

--- java/ArrayHashTable/ex1929.java
+++ java/ArrayHashTable/ex1929.java
@@ -1,4 +1,14 @@
 class ex1929 {
+    /**
+     * Given an integer array nums of length n, create an array ans of length 2n
+     * with a concatenation of nums with itself.
+     *
+     * Time Complexity: O(n) where n is the length of the input array.
+     * Space Complexity: O(n) for the output array.
+     *
+     * @param nums Array of integers
+     * @return Concatenated array of length 2n
+     */
     public int[] getConcatenation(int[] nums) {
         int[] ans = new int[nums.length*2];
         for (int i=0;i<nums.length;i++){
