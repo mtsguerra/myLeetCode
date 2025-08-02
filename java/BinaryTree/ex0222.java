2c5064afe9a65e87c24f3f7b76f31dd22f2eca9c
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-02 23:15:40 +0100

    Count Complete Tree Nodes - Adding comments

--- java/BinaryTree/ex0222.java
+++ java/BinaryTree/ex0222.java
@@ -11,11 +11,21 @@ public class ex0222 {
             this.right = right;
         }
     }
-    
+
+    /**
+     * Given the root of a complete binary tree, return the number of the
+     * nodes in the tree. A complete binary tree is a binary tree in which
+     * every level, except possibly the last, is completely filled, and all
+     * nodes are as far left as possible.
+     *
+     * Time Complexity: O(n) where n is the number of nodes in the tree.
+     * Space Complexity: O(h) where h is the height of the tree due to recursion stack.
+     *
+     * @param root Root node of the binary tree
+     * @return Number of nodes in the complete binary tree
+     */
     public int countNodes(TreeNode root) {
-        // base case if current root == null 
         if (root==null) return 0;
-        // recursive call  
         else return 1 + countNodes(root.left) + countNodes(root.right);
     }
                
