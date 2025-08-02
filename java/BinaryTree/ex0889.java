fb9839f9e0cd981d462032e0af885917bc02ed4f
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-02 23:19:12 +0100

    Construct Binary Tree from Preorder and Postorder Traversal - Adding comments

--- java/BinaryTree/ex0889.java
+++ java/BinaryTree/ex0889.java
@@ -1,7 +1,16 @@
-
-
-
 public class ex0889 {
+    /**
+     * Given two integer arrays preorder and postorder where preorder is the
+     * preorder traversal of a binary tree and postorder is the postorder
+     * traversal of the same tree, construct and return the binary tree.
+     *
+     * Time Complexity: O(n) where n is the number of nodes in the tree.
+     * Space Complexity: O(n) for storing the tree nodes.
+     *
+     * @param preorder Preorder traversal of the binary tree
+     * @param postorder Postorder traversal of the binary tree
+     * @return The root node of the constructed binary tree
+     */
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
         return buildTree(preorder, 0, preorder.length - 1,
                 postorder, 0, postorder.length - 1);
@@ -9,19 +18,13 @@ public class ex0889 {
 
     private TreeNode buildTree(int[] preorder, int preStart, int preEnd,
                                int[] postorder, int postStart, int postEnd) {
-        // Base case: if invalid indices or empty array
         if (preStart > preEnd) {
             return null;
         }
-
-        // Create root node from first element of preorder
         TreeNode root = new TreeNode(preorder[preStart]);
-
-        // If only one node left
         if (preStart == preEnd) {
             return root;
         }
-
         // Find the index of next element (preorder[preStart + 1]) in postorder
         // This will help us determine the left subtree size
         int leftRootIdx = -1;
@@ -31,16 +34,13 @@ public class ex0889 {
                 break;
             }
         }
-
         // Calculate size of left subtree
         int leftSubtreeSize = leftRootIdx - postStart + 1;
-
         // Recursively construct left and right subtrees
         root.left = buildTree(preorder, preStart + 1, preStart + leftSubtreeSize,
                 postorder, postStart, leftRootIdx);
         root.right = buildTree(preorder, preStart + leftSubtreeSize + 1, preEnd,
                 postorder, leftRootIdx + 1, postEnd - 1);
-
         return root;
     }
 
