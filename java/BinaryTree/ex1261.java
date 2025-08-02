3e8b9b2565d7e072926543490a6629066d59bc68
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-02 23:22:41 +0100

    Find Elements in a Contaminated Binary Tree - Adding comments

--- java/BinaryTree/ex1261.java
+++ java/BinaryTree/ex1261.java
@@ -1,23 +1,21 @@
+import java.util.*;
+
 class ex1261 {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
 
     class FindElements {
-
         private Set<Integer> values;
-
         public FindElements(TreeNode root) {
             values = new HashSet<>();
             if (root!=null) {
