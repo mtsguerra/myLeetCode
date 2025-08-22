f809fe9969da327f65a5f8e168393d6e8c843138
Matheus Guerra <mtsguerra9@gmail.com>
2025-08-22 22:28:26 +0100

    Find the Minimum Area to Cover All Ones I - Updating comments

--- java/ArrayHashTable/ex3195.java
+++ java/ArrayHashTable/ex3195.java
@@ -1,6 +1,16 @@
 import java.util.*;
 
 class ex3195{
+    /**
+     * Given a binary grid, return the area of the smallest axis-aligned rectangle that encloses all the 1's in the grid.
+     * If there are no 1's in the grid, return 0.
+     *
+     * Time Complexity: O(m * n), where m is the number of rows and n is the number of columns in the grid.
+     * Space Complexity: O(1), as we are using only a constant amount of extra space.
+     *
+     * @param grid a binary grid
+     * @return the area of the smallest axis-aligned rectangle that encloses all the 1's in the grid
+     */
     public int minimumArea(int[][] grid) {
         int minY = Integer.MAX_VALUE, maxY = Integer.MIN_VALUE;
         int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
