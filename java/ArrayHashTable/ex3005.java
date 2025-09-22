94c38cfcaa95b4b29951d61b58e0a7046af7f535
Matheus Guerra <mtsguerra9@gmail.com>
2025-09-22 12:12:15 +0100

    Adding ex3005. Count Elements With Maximum Frequency

---
+++ java/ArrayHashTable/ex3005.java
@@ -0,0 +1,29 @@
+class ex3005 {
+    /**
+     * You are given an integer array nums.The frequency of an element is
+     * the number of times it appears in the array.
+     *
+     * Time Complexity: O(n), where n is the length of the input array.
+     * Space Complexity: O(1), since the size of the frequency array is fixed (
+     *
+     * @param nums an integer array
+     * @return the number of elements that have the maximum frequency in the
+     * array
+     */
+    public int maxFrequencyElements(int[] nums) {
+        int[] arr = new int[101];
+        int ans = 0;
+        int biggestSeq = 0;
+        for (int n : nums){
+            arr[n]++;
+            if (arr[n] > biggestSeq){
+                biggestSeq = arr[n];
+                ans = arr[n];
+            }
+            else if (arr[n] == biggestSeq) {
+                ans += arr[n];
+            }
+        }
+        return ans;
+    }
+}
