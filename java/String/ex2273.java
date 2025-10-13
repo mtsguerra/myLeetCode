95ff6da8c5357b37a5ccf6ca8a6ab86830f40b9c
Matheus Guerra <mtsguerra9@gmail.com>
2025-10-13 11:35:40 +0100

    Find Resultant Array After Removing Anagrams - Updating comms

--- java/String/ex2273.java
+++ java/String/ex2273.java
@@ -1,6 +1,16 @@
 import java.util.*;
 
 class ex2273 {
+    /**
+     * Given an array of strings words, remove the consecutive strings that are anagrams of each other and return the remaining array.
+     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+     *
+     * Time Complexity: O(n * m), where n is the number of words and m is the average length of the words.
+     * Space Complexity: O(n), for storing the result list and frequency arrays.
+     *
+     * @param words an array of strings
+     * @return the array after removing consecutive anagrams
+     */
     public List<String> removeAnagrams(String[] words) {
         List<String> res = new ArrayList<>();
         res.add(words[0]);
