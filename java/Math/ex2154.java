04e3983ac0aca93c559e2afe3517b0e06bb9f40a
Matheus Guerra <mtsguerra9@gmail.com>
2025-11-19 11:51:53 +0000

    Keep Multiplying Found Values by Two - adding solution

--- java/Math/ex2154.java
+++ java/Math/ex2154.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 class ex2154 {
+    /**
+     * Find the final value after doubling original if it exists in nums
+     *
+     * @param nums array of integers
+     * @param original the original integer
+     * @return the final value after doubling
+     */
     public int findFinalValue(int[] nums, int original) {
         boolean[] set = new boolean[1001];
         for (int num : nums){
